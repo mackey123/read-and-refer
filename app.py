ed265f96edcff3babbcfaf21a7b903f4cb2d1c2e
mackey123 <seanrobert2010@gmail.com>
2021-09-14 15:34:35 +0000

    fix error of code on line 11 in profile.html file

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ def get_books():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        
+
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            flash("username already exists")   
+            flash("username already exists")
             return redirect(url_for("register"))
 
         register = {
@@ -44,9 +44,10 @@ def register():
         mongo.db.users.insert_one(register)
 
         session["user"] = request.form.get("username").lower()
-        flash("Registration successful!")
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
-    
+ 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -54,22 +55,36 @@ def login():
         # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
+        
+        
         if existing_user:
+            # 
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "profile", username=session["user"]))
             else:
+                # password incorrect
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
 
         else:
+            # unknown username
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
 
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 
